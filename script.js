688d675086750f05e796028e0dbd796b4108bddf
Zenith Kandel <kandelze123@gmail.com>
2025-09-23 07:38:21 +0545

    feat: implement particle effects in preloader for enhanced visual dynamics

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 /**/
+function loadParticles(parent) {
+    const particles = parent.getElementsByClassName('particle');
+    console.log(particles);
+    Array.from(particles).forEach(particle => {
+        particle.style.fontSize = Math.random() * 32 +20 + 'px';
+    });
+}
+
+let particles_container = document.querySelector('.particles');
+loadParticles(particles_container);
