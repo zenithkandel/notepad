ceacb30912d846b20bec19c73023d293a7bc8e87
Zenith Kandel <kandelze123@gmail.com>
2025-09-23 07:45:50 +0545

    fix: remove transition from particle elements for smoother animation

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-/**/
+const particles_container = document.querySelector('.particles');
+// initialize particle position before animating
+loadParticles(particles_container);
+
+// animate the particles position every 10 seconds
 function loadParticles(parent) {
+    const containerWidth = parent.offsetWidth;
+    const containerHeight = parent.offsetHeight;
     const particles = parent.getElementsByClassName('particle');
     console.log(particles);
     Array.from(particles).forEach(particle => {
-        particle.style.fontSize = Math.random() * 32 +20 + 'px';
-        particle.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        particle.style.fontSize = Math.random() * 32 + 20 + 'px';
+        // particle.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        newX = Math.random() * containerWidth;
+        newY = Math.random() * containerHeight;
+        particle.style.transform = `translate(${newX}px, ${newY}px)`;
     });
 }
 
-let particles_container = document.querySelector('.particles');
-loadParticles(particles_container);
+setInterval(() => {
+    loadParticles(particles_container);
+}, 10000);
