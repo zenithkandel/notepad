750de60af6839d29a62be0acaf23d9d6c2669467
Zenith Kandel <kandelze123@gmail.com>
2025-09-23 07:40:11 +0545

    fix: adjust particle opacity in preloader for improved visibility

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function loadParticles(parent) {
     console.log(particles);
     Array.from(particles).forEach(particle => {
         particle.style.fontSize = Math.random() * 32 +20 + 'px';
+        particle.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
     });
 }
 
