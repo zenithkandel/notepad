9e45264a46d5afeeb3b9a112bdf497ce49b7973a
Zenith Kandel <kandelze123@gmail.com>
2025-09-23 10:53:34 +0545

    fix: streamline particle loading process by removing unnecessary delays and transitions

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const particles_container = document.querySelector('.particles');
-// initialize particle position before animating
-loadParticles(particles_container);
-setTimeout(() => {
-Array.from(particles_container.getElementsByClassName('particle')).forEach(particle => {
-    particle.style.transition = 'all 50s linear';
-});
-    loadParticles(particles_container);
 
-}, 1000);
-// animate the particles position every 10 seconds
 function loadParticles(parent) {
     const containerWidth = parent.offsetWidth;
     const containerHeight = parent.offsetHeight;
@@ -16,13 +7,10 @@ function loadParticles(parent) {
     console.log(particles);
     Array.from(particles).forEach(particle => {
         particle.style.fontSize = Math.random() * 50 + 40 + 'px';
-        // particle.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+        particle.style.color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
         newX = Math.random() * containerWidth;
         newY = Math.random() * containerHeight;
         particle.style.transform = `translate(${newX}px, ${newY}px)`;
     });
 }
-
-setInterval(() => {
-    loadParticles(particles_container);
-}, 50000);
+loadParticles(particles_container);
